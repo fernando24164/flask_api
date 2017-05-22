7a8605844cacf836364a35d7e59ca076ad36998c
Fernando Cillero <fernando.cillero@a3sec.com>
2017-05-22 16:33:32 +0200

    Update models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,16 @@
 from . import db
 
 
+association_table = db.Table('association', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('station_id', db.Integer, db.ForeignKey('stations.id'))
+)
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(55), unique=True, index=True)
-    weather_station = db.relationship('Weather_Station', backref='meteorologist',
-                                      lazy='dynamic')
 
     def __repr__(self):
         return '<User %s>' % self.name
@@ -32,7 +36,8 @@ class Weather_Station(db.Model):
     description = db.Column(db.String(255), unique=True)
     latitude = db.Column(db.String(255))
     longitude = db.Column(db.String(255))
-    users = db.Column(db.Integer, db.ForeignKey('users.id'))
+    users = db.relationship("User",
+                            secondary=association_table)
 
     def __repr__(self):
         return '<Weather_Station %s>' % self.description
@@ -48,6 +53,9 @@ class Weather_Station(db.Model):
                                    str(forgery_py.geo.latitude_direction()),
                                    longitude=str(forgery_py.geo.longitude_degrees()) +
                                    str(forgery_py.geo.longitude_direction()))
+            users = User.query.all()
+            for user in users:
+                data.users.append(user)
 
             db.session.add(data)
             try:
