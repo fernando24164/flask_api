594869bca15b93a8e583e29b1a65496bd6c058f2
Fernando Cillero <fernando.cillero@a3sec.com>
2017-05-24 09:17:13 +0200

    Add SQLALCHEMY_TRACK_MODIFICATION to supress warnings

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'f63f65a3f7274455bfd49edf9c6b36bd'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
