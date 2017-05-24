5a0e935f99cc83c072e257939b25b6273c880c69
Fernando Cillero <fernando.cillero@a3sec.com>
2017-05-24 10:09:35 +0200

    Update bootrap to vagratn machine

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,11 @@ APT_OPTS="-uy -q --force-yes --allow-unauthenticated --no-install-recommends"
 
 apt-get update $APT_OPTS
 
-# install vim
 apt-get install vim $APT_OPTS
 
-apt-get install sqlite3 libsqlite3-dev
+apt-get install build-essential python3-dev python3-setuptools python3-pip $APT_OPTS
 
-apt-get install build-essential python3-dev python3-setuptools $APT_OPTS
-
-apt-get install python3-pip $APT_OPTS
+apt-get install ipython $APT_OPTS
 
 pip3 install -r /vagrant/app/requirements.txt
 
