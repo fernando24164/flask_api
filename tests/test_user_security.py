b5132bbc3c686ac146861d4005deda4f56fdd438
Fernando Cillero <fernando.cillero@a3sec.com>
2017-05-25 11:04:29 +0200

    Add more tests

--- tests/test_user_security.py
+++ tests/test_user_security.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
 from app import create_app, db
-from app.api.models import User, Weather_Station, Forecast
-
+from app.api.models import User
+from base64 import b64encode
+import json
 
 
 class UserModelTestCase(TestCase):
@@ -11,17 +12,57 @@ class UserModelTestCase(TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-        self.client = self.app.test_client(use_cookies=True)
+        self.client = self.app.test_client()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
+    def get_api_headers(self, name, passwd):
+        return {
+            'Authorization': 'Basic ' + b64encode(
+                (name + ':' + passwd).encode('utf-8')).decode('utf-8'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
     def test_password(self):
         user = User(name='admin', pwd='admin')
         self.assertTrue(user.verify_password('admin'))
 
-    def test_url(self):
-        response = self.client.get('/api/test/')
+    def test_endpoint(self):
+        response = self.client.get('/api/test')
+        self.assertTrue(response.status_code == 200)
+
+    def test_secret_endpoint(self):
+        response = self.client.get('/api/secret')
+        self.assertTrue(response.status_code == 403)
+
+    def test_token_based_authentication(self):
+        u = User(name='Donald Trump', pwd='1234')
+        db.session.add(u)
+        db.session.commit()
+
+        response = self.client.get('/api/secret',
+                                   headers=self.get_api_headers('Donald Trump',
+                                                                '1234'))
+        self.assertTrue(response.status_code == 200)
+
+        response = self.client.get('/api/secret',
+                                   headers=self.get_api_headers('hack',
+                                                                '1234'))
+        self.assertTrue(response.status_code == 403)
+
+        response = self.client.get(
+            '/api/token',
+            headers=self.get_api_headers('Donald Trump', '1234'))
+        self.assertTrue(response.status_code == 200)
+        json_response = json.loads(response.data.decode('utf-8'))
+        self.assertIsNotNone(json_response.get('token'))
+        token = json_response['token']
+
+        response = self.client.get(
+            '/api/secret',
+            headers=self.get_api_headers(token, ''))
         self.assertTrue(response.status_code == 200)
